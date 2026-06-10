3f7c890ec1f2bb8989887c95fc74426ef9950d16
PauSubdelegado <paumorant19@gmail.com>
2026-06-10 13:39:35 +0200

    QUE ES ESTO TU

---
+++ src/Router.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom';
+import ProtectedRoute from './guards/ProtectedRoute';
+// layout
+import Layout from './pages/Layout';
+// Pages
+import Login from './pages/Login';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Registro from './pages/Registro';
+import AreaPersonal from './pages/AreaPersonal';
+
+
+
+export const Router = createBrowserRouter([
+    {
+        path:'/',
+        element: <Layout/>,
+        children:[
+            {index: true, element: <Login/>},
+            {path: 'login', element:<Login/>},
+            {path: 'registro', element:<Registro />},
+        // zona protegida
+            {
+                element: <ProtectedRoute />,
+                children:[
+                    {path:"home", element: <Home/>},
+                    {path:"areapersonal", element: <AreaPersonal/>}
+                ]
+            },
+            // error 404
+            {path:"*", element:<NotFound/>}
+        ]
+    }
+])
+export default Router
